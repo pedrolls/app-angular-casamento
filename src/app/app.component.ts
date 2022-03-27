f541f113bfbbbd9ca28b96584006a2943d0182a3
pedrolls <silva418@hotmail.com>
2022-03-27 20:01:35 -0300

    ajustes na toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private router: Router) { }
+
   title = 'casamento';
+
+  redirectHome(): void{
+    this.router.navigateByUrl('/home');
+  }
+
+  redirectSobreOsNoivos(): void{
+    this.router.navigateByUrl('/home/sobre-os-noivos')
+  }
 }
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,9 +1,11 @@
+import { SobreOsNoivosComponent } from './sobre-os-noivos/sobre-os-noivos.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'home/sobre-os-noivos', component: SobreOsNoivosComponent }
 ];
 
 @NgModule({
